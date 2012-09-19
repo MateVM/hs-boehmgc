850ed5efbfc0d6b019c42970fa4cc1a40e2fe87f
Bernhard Urban <lewurm@gmail.com>
2012-09-19 13:02:29 +0200

    configure: explicitly build for i386

--- makeboehm.sh
+++ makeboehm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd gc-7.2
-./configure --prefix=$1 --disable-threads
+CC='gcc -m32' ./configure --prefix=$1 --disable-threads --target=i386-unknown-linux-gnu
 make 
 make install
